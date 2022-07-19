de5644c53168007e588387aca804e5900442e103
Riddick <danijel.galic@live.com>
2022-07-19 12:09:08 +0200

    Created suffix and prefix for filetypes (#5)

---
+++ core/initializers/enums/firehub.Prefix.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of FireHub Web Application Framework package.
+ * @since 0.1.4.pre-alpha.M1
+ *
+ * @author Danijel Galić
+ * @copyright 2020 FireHub Web Application Framework
+ * @license OSL Open Source License version 3 - [https://opensource.org/licenses/OSL-3.0](https://opensource.org/licenses/OSL-3.09
+ *
+ * @version 1.0
+ * @package FireHub\Initializers
+ */
+
+namespace FireHub\Initializers\Enums;
+
+/**
+ * ### Enum for available prefixes for file types
+ * @since 0.1.4.pre-alpha.M1
+ *
+ * @package FireHub\Initializers
+ */
+enum Prefix:string {
+
+    case FIREHUB = 'firehub';
+
+}
