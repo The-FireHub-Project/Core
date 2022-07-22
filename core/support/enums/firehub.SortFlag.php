ccd054e76299df8c789d9a3cfab4334d9928a074
Riddick <danijel.galic@live.com>
2022-07-22 13:28:06 +0200

    Created SortFlag enum (#8)

---
+++ core/support/enums/firehub.SortFlag.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of FireHub Web Application Framework package.
+ * @since 0.2.0.pre-alpha.M2
+ *
+ * @author Danijel Galić
+ * @copyright 2022 FireHub Web Application Framework
+ * @license OSL Open Source License version 3 - [https://opensource.org/licenses/OSL-3.0](https://opensource.org/licenses/OSL-3.0)
+ *
+ * @package FireHub\Support\Enums
+ * @version 1.0
+ */
+
+namespace FireHub\Support\Enums;
+
+/**
+ * ### Sorting collection enum
+ * @since 0.2.0.pre-alpha.M2
+ *
+ * @package FireHub\Support\Enums
+ */
+enum SortFlag:int {
+
+    /**
+     * ### Compare items normally
+     * @since 0.2.0.pre-alpha.M2
+     */
+    case SORT_REGULAR = 0;
+
+    /**
+     * ### Compare items numerically
+     * @since 0.2.0.pre-alpha.M2
+     */
+    case SORT_NUMERIC = 1;
+
+    /**
+     * ### Compare items as strings
+     * @since 0.2.0.pre-alpha.M2
+     */
+    case SORT_STRING = 2;
+
+    /**
+     * ### Compare items as strings, based on the current locale. It uses the locale, which can be changed using setlocale()
+     * @since 0.2.0.pre-alpha.M2
+     */
+    case SORT_LOCALE_STRING = 5;
+
+    /**
+     * ### Compare items as strings using "natural ordering" like natsort()
+     * @since 0.2.0.pre-alpha.M2
+     */
+    case SORT_NATURAL = 6;
+
+    /**
+     * ### Sort strings case-insensitively
+     * @since 0.2.0.pre-alpha.M2
+     */
+    case SORT_STRING_FLAG_CASE = 10;
+
+    /**
+     * ### Sort natural case-insensitively
+     * @since 0.2.0.pre-alpha.M2
+     */
+    case SORT_NATURAL_FLAG_CASE = 14;
+
+}
