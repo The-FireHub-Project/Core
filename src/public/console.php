7f9b6a051c5573af70e920aca0eeca2db3880b28
Riddick <danijel.galic@live.com>
2024-11-11 15:48:00 +0100

    Create console.php (#3)
    This is a landing file if the PHAR archive is accessed through a command-line.

---
+++ src/public/console.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the FireHub Web Application Framework package
+ *
+ * This is a landing file if the PHAR archive is accessed through a command-line.
+ * @since 1.0.0
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2025 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package Core\Public
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
