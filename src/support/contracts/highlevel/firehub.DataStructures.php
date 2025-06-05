3687ae48dcd1a4fa3f6618733d60fe4026cb36c3
riddick <danijel.galic@live.com>
2025-06-05 08:12:11 +0200

    Create data structures contracts (#18)

---
+++ src/support/contracts/highlevel/firehub.DataStructures.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the FireHub Web Application Framework package
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2025 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package Core\Support
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
+
+namespace FireHub\Core\Support\Contracts\HighLevel;
+
+/**
+ * ### Data structures
+ * @since 1.0.0
+ *
+ * @template TKey
+ * @template TValue
+ *
+ * @extends \FireHub\Core\Support\Contracts\Iterator\IteratorAggregate<TKey, TValue>
+ */
+interface DataStructures {
+
+}
