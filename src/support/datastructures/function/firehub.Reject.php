aeaca2b14946aa4afead5b82be419149892b8235
riddick <danijel.galic@live.com>
2025-06-23 13:39:22 +0200

    Create reject and partition functions for data structures (#18)

---
+++ src/support/datastructures/function/firehub.Reject.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the FireHub Web Application Framework package
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2025 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package Core\Support
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
+
+namespace FireHub\Core\Support\DataStructures\Function;
+
+use FireHub\Core\Support\DataStructures\Contracts\Filterable;
+
+/**
+ * ### Reject items from data structure
+ * @since 1.0.0
+ *
+ * @template TDataStructure of \FireHub\Core\Support\DataStructures\Contracts\Filterable
+ */
+readonly class Reject {
+
+    /**
+     * ### Constructor
+     * @since 1.0.0
+     *
+     * @uses \FireHub\Core\Support\DataStructures\Contracts\Filterable As parameter.
+     *
+     * @param TDataStructure $data_structure <p>
+     * Instance of data structures.
+     * </p>
+     *
+     * @return void
+     */
+    public function __construct (
+        protected Filterable $data_structure
+    ) {}
+
+    /**
+     * ### Call an object as a function
+     * @since 1.0.0
+     *
+     * @uses \FireHub\Core\Support\DataStructures\Contracts\Filterable::filter() To filter items from the data structure.
+     *
+     * @param callable(value-of<TDataStructure>, key-of<TDataStructure>):bool $callback <p>
+     * Function to call on each item in a data structure.
+     * </p>
+     *
+     * @return TDataStructure New filtered data structure.
+     */
+    public function __invoke (callable $callback):Filterable {
+
+        // @phpstan-ignore return.type
+        return $this->data_structure->filter(fn($value, $key) => !$callback($value, $key)); // @phpstan-ignore argument.type
+
+    }
+
+}
